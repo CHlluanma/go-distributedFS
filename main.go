0305b0f23e110b372a290c37f6f33faab992c9d5
ahang <ch1447571882@qq.com>
2024-07-20 22:16:00 +0800

    ✨ feat: server run

--- main.go
+++ main.go
@@ -3,34 +3,45 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/ahang7/go-distributedFS/p2p"
 )
 
-func OnPeer(peer p2p.Peer) error {
-	fmt.Println("OnPeer", peer)
-	return nil
-}
-
-func main() {
+func makeServer(listenAddr, root string, nodes ...string) *FileServer {
 	tcpOpts := p2p.TCPTransportOpts{
-		ListenAddr:    ":3000",
+		ListenAddr:    listenAddr,
 		HandshakeFunc: p2p.NOPHandshakeFunc(),
 		Decoder:       p2p.DefaultDecoder{},
-		OnPeer:        OnPeer,
+		OnPeer: func(p p2p.Peer) error {
+			fmt.Printf("OnPeer: %+v", p)
+			return nil
+		},
 	}
 	tr := p2p.NewTCPTransport(tcpOpts)
 
 	fileServerOpts := FileServerOpts{
-		StorageRoot:       "3000_network",
+		StorageRoot:       root,
 		PathTransformFunc: CASPathTransformFunc,
 		Transport:         tr,
+		BootstrapNetWork:  nodes,
 	}
 
 	s := NewFileServer(fileServerOpts)
+	tr.OnPeer = s.OnPeer
+
+	return s
+}
+
+func main() {
+	s := makeServer(":3000", "3000_network", ":4000")
+
+	go func() {
+		time.Sleep(time.Second * 5)
+		s.Stop()
+	}()
+
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
-
-	select {}
 }
--- server.go
+++ server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"io"
+	"log"
+	"sync"
 
 	"github.com/ahang7/go-distributedFS/p2p"
 )
@@ -11,12 +13,17 @@ type FileServerOpts struct {
 	StorageRoot       string
 	PathTransformFunc PathTransformFunc
 	Transport         p2p.Transport
+	BootstrapNetWork  []string
 }
 
 type FileServer struct {
 	FileServerOpts
 
-	store *Store
+	peerLock sync.Mutex
+	peers    map[string]p2p.Peer
+
+	store  *Store
+	quitch chan struct{}
 }
 
 func NewFileServer(opts FileServerOpts) *FileServer {
@@ -28,17 +35,70 @@ func NewFileServer(opts FileServerOpts) *FileServer {
 	return &FileServer{
 		FileServerOpts: opts,
 		store:          NewStore(storeOpts),
+		quitch:         make(chan struct{}),
+		peers:          make(map[string]p2p.Peer),
 	}
 }
 
-func (s *FileServerOpts) Start() error {
+func (s *FileServer) Start() error {
 	if err := s.Transport.ListenAndAccept(); err != nil {
 		return err
 	}
 
+	if len(s.BootstrapNetWork) != 0 {
+		s.bootstrapNetWork()
+	}
+	s.loop()
+
 	return nil
+}
+
+func (s *FileServer) Stop() {
+	close(s.quitch)
 }
 
 func (s *FileServer) Store(key string, r io.Reader) error {
 	return s.store.Write(key, r)
+}
+
+func (s *FileServer) OnPeer(p p2p.Peer) error {
+	s.peerLock.Lock()
+	defer s.peerLock.Unlock()
+
+	s.peers[p.RemoteAddr().String()] = p
+
+	log.Printf("connected with remote %s", p.RemoteAddr())
+
+	return nil
+}
+
+func (s *FileServer) loop() {
+	defer func() {
+		log.Println("file server stop due to user quit action")
+		s.Transport.Close()
+	}()
+
+	for {
+		select {
+		case msg := <-s.Transport.Consume():
+			log.Println(msg)
+		case <-s.quitch:
+			return
+		default:
+			// log.Println("default loop ....")
+		}
+	}
+}
+
+func (s *FileServer) bootstrapNetWork() error {
+	for _, addr := range s.BootstrapNetWork {
+		log.Println("bootstrapping the network")
+		go func(addr string) {
+			if err := s.Transport.Dial(addr); err != nil {
+				log.Println(err)
+			}
+		}(addr)
+	}
+
+	return nil
 }
